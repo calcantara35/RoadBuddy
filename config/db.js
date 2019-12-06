15a35ac592fcd7a32b600f2233309c07553839c3
Cesar Alcantara <cja5391@psu.edu>
2019-12-06 14:34:40 -0500

    connected mongodb

---
+++ config/db.js
@@ -0,0 +1,25 @@
+// mongo db connection
+const mongoose = require("mongoose");
+const config = require("config");
+
+// using config to bring in mongoURI value from default.json file
+const db = config.get("mongoURI");
+
+// using async await here. always wrap logic inside try catch block
+const connectDB = async () => {
+  try {
+    // this begins the DB connection. "mongoose.connect(db)" returns a promise so you put await before it
+    // reason for useNewUrlParser and useUnifiedTopology is because without them you get a deprecation wanring. MongoDB standard
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+/* 
+When you finally pull the updates, run npm install then
+to run app, npm run server.
+You must do this all in the master branch
+To switch branches use: git checkout <branchname> (dont include <>)
+
+once your master branch is updated, do the following steps: 
+1) git checkout <yourBranchName>
+
+2) git merge master
+
+*/
+
+//bring in express
 const express = require("express");
 
+// bringing in connectDB method from db.js
+const connectDB = require("./config/db");
+
 const server = express();
+
+//connect Database
+connectDB();
 
 server.get("/", (req, res) => {
   res.send("API Data");
